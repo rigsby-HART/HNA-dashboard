61f2e2369a20cc1df4f63b1f20a0529107d9bffe
Jack-Mei-UBC <jackmeimei@gmail.com>
2023-10-17 10:10:20 -0700

    Implemented Disk Caching
    - Current app has no caching layer, this implements a simple caching system on the Dash Server level.  Optimally could be implemented higher.

--- app_file.py
+++ app_file.py
@@ -3,8 +3,14 @@
 
 import dash
 import dash_bootstrap_components as dbc
-
+import diskcache
+from dash import DiskcacheManager
+cache = diskcache.Cache("./cache")
+background_callback_manager = DiskcacheManager(
+    cache, cache_by=[], expire=300
+)
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
                 meta_tags=[{"name": "viewport", "content": "width=device-width"}],
-                suppress_callback_exceptions=True)
+                suppress_callback_exceptions=True,
+                background_callback_manager = background_callback_manager)
